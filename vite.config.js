34ee26b963d3c32ecc4e585630b302c0293bb163
Omar Alhamad <omar93@hotmail.se>
2023-03-26 14:06:01 +0200

    saving config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: 3030,
+	}
 };
 
 export default config;
